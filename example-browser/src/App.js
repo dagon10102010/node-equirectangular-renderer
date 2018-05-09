02c7efc07ab8d499e5ac15ab0a7cfb47330abe2a
mark <mark.korput@gmail.com>
2018-05-09 11:07:56 +0200

    toggle-able equi background and 3d objects without compromising equi render

--- example-browser/src/App.js
+++ example-browser/src/App.js
@@ -23,28 +23,43 @@ class App extends Component {
 
   componentDidMount() {
 
+    this.bgTex = new THREE.TextureLoader().load( '2294472375_24a3b8ef46_o.jpg' );
+
+    this.renderer = new THREE.WebGLRenderer( { antialias: true });
+    this.renderer.setSize(800,600);
+    const el = document.getElementsByClassName('three-scene')[0];
+    el.appendChild(this.renderer.domElement);
 
     createTexturedPlaneRenderer({image: 'UV_Grid_Sm.jpg', winWidth: 800, winHeight: 600, resolution: [1024,512], translate: [0,0,-20], scale: [1,0.5,0.5]})
     .then((ctx) => {
       // console.log('createTexturedPlaneRenderer done: ', ctx);
-
       this.ctx = ctx;
-      this.ctx.renderer.setSize(800, 600);
-
-      const el = document.getElementsByClassName('three-scene')[0];
-      el.appendChild(this.ctx.renderer.domElement);
+      // this.ctx.renderer.setSize(800, 600);
+      // this.ctx.plane.material.depthTest=false;
+      this.ctx.plane.material.side = THREE.DoubleSide;
+      // this.ctx.scene.background = this.bgTex;
+      // const el = document.getElementsByClassName('three-scene')[0];
+      // el.appendChild(this.ctx.renderer.domElement);
       // document.getElementById('three-scene').appendChild(this.ctx.renderer.domElement);
 
+      // this.scene = new THREE.Scene();
+      // this.ctx.scene.children.forEach((obj) => this.scene.add(obj));
+      this.scene = this.ctx.scene.clone();
+
       this.bg = this.createBackground()
-      // this.ctx.scene.add(this.bg);
+      // this.scene.add(this.bg);
 
       this.cubegeometry = new THREE.BoxGeometry( 0.1, 0.1, 0.1 );
       this.cubematerial = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+      this.cubematerial.depthTest = false;
+      this.cubematerial.side = THREE.DoubleSide;
       this.cubemesh = new THREE.Mesh( this.cubegeometry, this.cubematerial );
       this.cubemesh.position.set(5,0,-15);
       this.cubemesh.rotation.set(0.5,-0.2,0);
 
       this.cubematerial2 = new THREE.MeshBasicMaterial({ color: 0xff00ff });
+      this.cubematerial2.depthTest = false;
+      this.cubematerial2.side = THREE.DoubleSide;
       this.cubemesh2 = new THREE.Mesh( this.cubegeometry, this.cubematerial2 );
       this.cubemesh2.position.set(-5,0,-15);
       this.cubemesh2.rotation.set(0.5,0.2,0);
@@ -68,16 +83,17 @@ class App extends Component {
     });
   }
 
-  addToScene(obj, add) {
-    if (add === undefined) add = this.ctx.scene.children.indexOf(obj) === -1;
-    if (add) { this.ctx.scene.add(obj); }
-    else { this.ctx.scene.remove(obj); }
+  addToScene(obj, scene, add) {
+    if (scene === undefined) scene = this.scene;
+    if (add === undefined) add = scene.children.indexOf(obj) === -1;
+    if (add) { scene.add(obj); }
+    else { scene.remove(obj); }
   }
 
   onKeyDown(e) {
     // console.log('keydown', e);
 
-    if (e.key === '0' && this.ctx) this.addToScene(this.ctx.plane);
+    // if (e.key === '0' && this.ctx) this.addToScene(this.ctx.plane);
     if (e.key === '1' && this.ctx) this.addToScene(this.cubemesh);
     if (e.key === '2' && this.ctx) this.addToScene(this.cubemesh2);
     if (e.key === '9' && this.ctx) this.addToScene(this.bg);
@@ -92,18 +108,19 @@ class App extends Component {
   }
 
   render3d() {
-    if (this.ctx !== undefined) this.ctx.renderer.render(this.ctx.scene, this.ctx.camera);
+    if (this.ctx !== undefined) this.renderer.render(this.scene, this.ctx.camera);
   }
 
   createBackground(clr) {
-    let geometry = new THREE.SphereGeometry( 5, 60, 40 );
-    geometry.scale( - 1, 1, 1 );
+    let geometry = new THREE.SphereBufferGeometry( 5000, 60, 40 );
+    // geometry.scale( -1, 1, 1 );
 
-    // let material = new THREE.MeshBasicMaterial( {
-    //   map: new THREE.TextureLoader().load( '2294472375_24a3b8ef46_o.jpg' )
-    // });
-    let material = new THREE.MeshBasicMaterial({ color: clr || 0x00ffff });
-
+    let material = new THREE.MeshBasicMaterial( {
+      map: this.bgTex //new THREE.TextureLoader().load( '2294472375_24a3b8ef46_o.jpg' )
+    });
+    // let material = new THREE.MeshBasicMaterial({ color: clr || 0x00ffff });
+    // material.depthTest = false;
+    material.side = THREE.DoubleSide;
     let mesh = new THREE.Mesh( geometry, material );
     return mesh;
   }
